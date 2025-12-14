7c5ced2f2765ee7c94fe629cdbc2fd6dc8b48e98
Tucared <max.perdrigeat@gmail.com>
2025-12-14 03:04:43 -0800

    feat: remove sidebar

--- observablehq.config.js
+++ observablehq.config.js
@@ -1,13 +1,10 @@
 export default {
   title: "Expense Tracker",
   root: "src",
-  pages: [
-    { name: "Dashboard", path: "/" }
-  ],
   theme: "dashboard",
   toc: false,
   pager: false,
   footer: "",
   // Force Observable to use node_modules instead of CDN for npm: imports
-  npm: true
+  npm: true,
 };
